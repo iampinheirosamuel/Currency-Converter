76daf798d49b3646ce3f018f9edaba31dae9011a
iampinheirosamuel <pinheirolaoluwa@gmail.com>
2018-07-01 19:35:00 +0100

    Currency Converter Final- IDB missing

---
+++ js/script.js
@@ -0,0 +1,69 @@
+(function(){
+    //register service worker
+    if(!navigator.serviceWorker) return;
+
+    navigator.serviceWorker.register('/sw.js').then(function(){
+        console.log('Registered');
+    }).catch(function () {
+       console.log('Registration failed'); 
+    });
+
+
+    //fetch data from API
+    let url = "https://free.currencyconverterapi.com/api/v5/currencies";
+    $.get(url, function(data, status){
+        if(status === 'success'){
+           
+            for (let item in data.results) {
+
+                $('#c_from').append(
+                    `<option value="`+ item +`" >`+ item +`</option>`
+                );
+
+                $('#c_to').append(
+                    `<option value="` + item +`" >` + item + `</option>`
+                );
+                
+            }
+        }else{
+            console.log('something seem to be wrong');
+        }      
+    });
+
+    //get values from user input
+    $('#convert').click(function () {
+        const amount = $('#f_amount').val();
+            if (amount == null || amount === '' ){
+                alert('Enter amount ');
+            } else{
+               
+                let fromCurrency = encodeURIComponent($('#c_from').val());
+                let toCurrency = encodeURIComponent($('#c_to').val());
+                var query = fromCurrency + '_' + toCurrency;        
+
+                var urlResult = 'https://free.currencyconverterapi.com/api/v5/convert?q='
+                    + query+'&compact=y';
+
+                $.get(urlResult, function (data, status) {
+                    if (status === 'success') {
+                        let val = data[query].val;
+                        let total = val * amount;
+                        let result = total.toFixed(2);
+                        $('#result').val(result);
+                    } else {
+                        console.log('something went wrong');
+                    }
+
+                });
+
+            }        
+    });
+
+    //Store all queries
+
+
+
+  
+
+})();
+
---
+++ sw.js
@@ -0,0 +1,50 @@
+var staticCacheName = 'currency-static-v2';
+
+//install service worker and cache requests
+self.addEventListener('install', function(event){
+   
+   event.waitUntil(
+      caches.open(staticCacheName).then(function(cache){
+           return cache.addAll([
+               '/',
+               'js/script.js',
+               'js/jquery.min.js',
+               'css/style.css',
+               'images/bg-panel3.png',
+               'https://free.currencyconverterapi.com/api/v5/currencies'
+
+           ]);
+      })
+   )
+});
+
+//Activate service worker, delete previous version of the cache 
+self.addEventListener('activate',function (event) {
+    event.waitUntil(
+        caches.keys().then(function (cacheNames) {
+
+            return Promise.all(
+                cacheNames.filter(function (cacheName) {
+                    return cacheName.startsWith('currency-') &&
+                        cacheName != staticCacheName;
+                }).map(function(cacheName) {
+                    return cache.delte(cacheName);
+                })
+            );
+           
+        })
+    );
+});
+
+//listen for all requests within the service worker scope
+self.addEventListener('fetch', function(event) {
+     event.respondWith(
+       caches.match(event.request).then(function(response){
+           if(response) return response;
+           return fetch(event.response);    
+       }).catch(function () {
+           console.log('Bad request');
+       })
+     );
+    
+});
